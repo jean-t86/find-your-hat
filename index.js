2a31ca05427cc9b141a186387e11666c3607e905
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-23 19:59:39 +0400

    Added index.js and ran npm init in working directory

---
+++ index.js
@@ -0,0 +1,14 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+
+/**
+ * A class that represents the playing field
+ */
+class Field {
+
+}
