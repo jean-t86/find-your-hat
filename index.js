ae10646caf72f466dbeb1266a3fd42a5f08944a7
J.Tadebois <jpm.tadebois@gmail.com>
2020-12-25 18:16:49 +0400

    Added Field.generatedField() to generate a random field
    The current Field.generateField() method is non deterministic, i.e. it generates a field with no guarantees that the player can win the game.

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 /**
- * A class that represents the playing field
+ * A class that represents the playing field.
  */
 class Field {
   /**
-   * The class constructor
+   * The class constructor.
    * @param {array} field - a two dimentional array representing the playfield.
    */
   constructor(field) {
@@ -23,9 +23,9 @@ class Field {
    * Prints the field to the console.
    */
   print() {
-    for (let i = 0; i < this._field.length; i++) {
+    for (let y = 0; y < this._field.length; y++) {
       let line = '';
-      for (let j = 0; j < this._field[i].length; j++) {
+      for (let x = 0; j < this._field[x].length; x++) {
         line += this._field[i][j];
       }
       console.log(line);
@@ -37,7 +37,7 @@ class Field {
    * direction.
    * @param {array} direction - An array with exactly two elements representing
    * the x and ytranslation coordinates for the player's movement.
-   * @return {string} Either of three values: moved, won, or lost
+   * @return {string} Either of three values: moved, won, or lost.
    */
   move(direction) {
     this._x += direction[0];
@@ -60,9 +60,87 @@ class Field {
       return 'lost';
     }
   }
+
+  /**
+   * A method to generate a playing field.
+   * @param {number} height The height of the field.
+   * @param {number} width The width of the field.
+   * @param {number} percentageHoles The percentage of the field covered in
+   * holes.
+   * @return {arrray} A two-dimentional array representing the playing field.
+   */
+  static generateField(height, width, percentageHoles) {
+    const field = this.initializeField(height, width);
+
+    let randomIndex = this.getRandomIndex(height, width);
+    let x = randomIndex[0];
+    let y = randomIndex[1];
+    field[y][x] = pathCharacter;
+
+    randomIndex = this.getRandomIndex(height, width);
+    x = randomIndex[0];
+    y = randomIndex[1];
+    do {
+      if (field[y][x] === fieldCharacter) {
+        field[y][x] = hat;
+      } else {
+        randomIndex = this.getRandomIndex(height, width);
+        x = randomIndex[0];
+        y = randomIndex[1];
+      }
+    } while (field[y][x] !== hat);
+
+    const holesNeeded = Math.floor((percentageHoles / 100) * height * width);
+    for (let i = 0; i < holesNeeded; i++) {
+      randomIndex = this.getRandomIndex(height, width);
+      x = randomIndex[0];
+      y = randomIndex[1];
+      do {
+        if (field[y][x] === fieldCharacter) {
+          field[y][x] = hole;
+        } else {
+          randomIndex = this.getRandomIndex(height, width);
+          x = randomIndex[0];
+          y = randomIndex[1];
+        }
+      } while (field[y][x] !== hole);
+    }
+
+    return field;
+  }
+
+  /**
+   * A method to initialize a field with the {@see fieldCharacter}.
+   * @param {number} height The height of the field.
+   * @param {number} width The width of the field.
+   * @return {array} A two-dimentional array representing the field.
+   */
+  static initializeField(height, width) {
+    const field = [];
+    for (let y = 0; y < height; y++) {
+      field[y] = [];
+      for (let x = 0; x < width; x++) {
+        field[y][x] = fieldCharacter;
+      }
+    }
+    return field;
+  }
+
+  /**
+   * A method to return a random x and y coordinate on the field.
+   * @param {number} height The height of the field.
+   * @param {number} width The width of the field.
+   * @return {array} A random  x and y coordinate on the field.
+   */
+  static getRandomIndex(height, width) {
+    const index = [];
+    index[0] = Math.floor(Math.random() * width);
+    index[1] = Math.floor(Math.random() * height);
+    return index;
+  }
 }
 /**
- * A class to get the player's input
+ * A class to get the player's input.
  */
 class PlayerInput {
   /**
@@ -106,7 +184,7 @@ const field1 = [
  */
 class GameLogic {
   /**
-   * This method runs the game loop
+   * This method runs the game loop.
    */
   static run() {
     const field = new Field(field1);
